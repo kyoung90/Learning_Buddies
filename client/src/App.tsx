d86bd874f53667cf778140743721c9a103a964bf
Matthew McCormack <mmccormack1182@gmail.com>
2019-10-01 14:24:05 -0400

    added react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,27 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import userStore from './stores/userStore'
 import {useContext} from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 const App: React.FC = () => {
   const activityStore = useContext(userStore)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{activityStore.title}</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+
+      </div>
+    </Router>
   );
 }
 
