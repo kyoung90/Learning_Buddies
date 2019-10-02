d3796fa37701fb7c2ee04704dbcc5fa549520bcc
Matthew McCormack <mmccormack1182@gmail.com>
2019-10-02 15:57:19 -0400

    added Home, Profile, and Matched Users routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 import userStore from './stores/userStore'
 import {useContext} from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from './routes/Home'
+import Profile from './routes/Profile'
+import Matched from './routes/Matched'
 
 
 const App: React.FC = () => {
@@ -11,7 +14,28 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+            <li>
+              <Link to='/matched'>Matched Users</Link>
+            </li>
+          </ul>
+        </nav>
 
+
+
+
+
+
+        <Route exact path='/' component={Home}/>
+        <Route path='/profile' component={Profile}/>
+        <Route path='/matched' component={Matched}/>
 
       </div>
     </Router>
---
+++ client/src/routes/Matched.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Matched = () => {
+  return (
+    <div>
+      <h1>Matched Users Page</h1>
+    </div>
+  )
+}
+export default Matched
